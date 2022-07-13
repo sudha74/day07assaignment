6b5bb7ace873f9884243ff239a9c034a860d7f25
sudha123 <sudhanaukala1997@gmail.com>
2022-07-13 20:49:55 +0530

    sbmitted

---
+++ MainClass.java
@@ -0,0 +1,17 @@
+package com.Bridgelabz;
+
+public class SumExample {
+
+
+    void sum(){
+        int x = 10;
+        int y = 20;
+        int sum = x+y;
+        System.out.println("Sum is = "+sum);
+    }
+
+public static void main(String[] args) {
+
+    }
+}
+
---
+++ coin.java
@@ -0,0 +1,17 @@
+int headCount = 0;
+int tailCount = 0;
+
+    for(i=0; i < 1000; i++) {
+
+        if(r.nextInt(100)%2 == 0)
+        {
+            headCount++;
+        }
+        else
+        {
+            tailCount++;
+        }
+        System.out.println("Times head was flipped:" + headsCount);
+        System.out.println("Times tail was flippe:" + tailCount);
+
+        }
